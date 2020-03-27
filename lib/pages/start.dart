bec979541341ad8ea5b2e41552ee9028041c9780
MeirKlemp <251.klemp.work@gmail.com>
2020-03-27 16:04:40 +0300

    Created EnterMenu widget

--- lib/pages/start.dart
+++ lib/pages/start.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_fake/widgets/enter_menu.dart';
 
 class Start extends StatefulWidget {
   @override
@@ -6,18 +7,6 @@ class Start extends StatefulWidget {
 }
 
 class _LoginState extends State<Start> {
-  bool _signedUp = false;
-  Widget get _cannotSignUp {
-    if (_signedUp) {
-      return Text(
-        "Cannot Create New Account",
-        style: TextStyle(
-          color: Colors.red,
-        ),
-      );
-    }
-    return SizedBox.shrink();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,51 +19,9 @@ class _LoginState extends State<Start> {
               SizedBox.shrink(),
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 40.0, horizontal: 65.0),
-                      child: Image.asset("assets/images/title.png"),
-                    ),
-                    FlatButton(
-                      onPressed: () => setState(() => _signedUp = true),
-                      padding: const EdgeInsets.all(20.0),
-                      color: Colors.blue,
-                      child: Text(
-                        "Create New Account",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.1,
-                        ),
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(6.0),
-                          side: BorderSide(color: Colors.blue)),
-                    ),
-                    SizedBox(height: 8.0),
-                    FlatButton(
-                      onPressed: () {
-                        
-                      },
-                      padding: const EdgeInsets.all(20.0),
-                      color: Colors.transparent,
-                      child: Text(
-                        "Log In",
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.1,
-                        ),
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(6.0),
-                          side: BorderSide(color: Colors.transparent)),
-                    ),
-                    Center(child: _cannotSignUp),
-                  ],
+                child: EnterMenu(
+                  onLogin: () {},
+                  onSignup: () {},
                 ),
               ),
               Column(
---
+++ lib/widgets/enter_menu.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class EnterMenu extends StatelessWidget {
+  final void Function() onLogin;
+  final void Function() onSignup;
+
+  EnterMenu({Key key, this.onLogin, this.onSignup}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 65.0),
+          child: Image.asset("assets/images/title.png"),
+        ),
+        FlatButton(
+          onPressed: onSignup,
+          padding: const EdgeInsets.all(20.0),
+          color: Colors.blue,
+          splashColor: Colors.transparent,
+          child: Text(
+            "Create New Account",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.1,
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(6.0),
+              side: BorderSide(color: Colors.blue)),
+        ),
+        SizedBox(height: 8.0),
+        FlatButton(
+          onPressed: onLogin,
+          padding: const EdgeInsets.all(20.0),
+          color: Colors.transparent,
+          splashColor: Colors.transparent,
+          child: Text(
+            "Log In",
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.1,
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(6.0),
+              side: BorderSide(color: Colors.transparent)),
+        ),
+      ],
+    );
+  }
+}
